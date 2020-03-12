fc76878884398adf59640bf96168f42f4b42ae18
a-tione <heyanggo@gmail.com>
2020-03-12 16:58:13 +0800

    完成 Money页面样式与组件划分

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,23 +1,39 @@
 <template>
     <div>
-        <Layout>money</Layout>
+        <Layout>
+            <tags></tags>
+            <notes></notes>
+            <types></types>
+            <number-pad></number-pad>
+        </Layout>
     </div>
 </template>
 
 <script lang="ts">
+    import notes from '@/components/Money/Notes.vue'
+    import numberPad from '@/components/Money/NumberPad.vue'
+    import tags from '@/components/Money/Tags.vue'
+    import types from '@/components/Money/Types.vue'
+
     export default {
         name: 'money',
-        props: {},
-
+        components: {
+            tags,
+            notes,
+            types,
+            numberPad,
+        },
         data() {
             return {}
         },
-
 
         methods: {}
     }
 </script>
 
 <style lang="scss" scoped>
-
+    .layout-content {
+        display: flex;
+        flex-direction: column-reverse;
+    }
 </style>
