2690e1469310ceab7ca61b7fd92b0bfeb4790250
a-tione <heyanggo@gmail.com>
2020-03-12 11:24:17 +0800

    创建icon，封装icon组件

---
+++ src/components/Icon.vue
@@ -0,0 +1,47 @@
+<template>
+    <div>
+        <svg class="icon">
+            <use :xlink:href="'#' + name"></use>
+        </svg>
+    </div>
+</template>
+
+<script lang="ts">
+    const importAll = (requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext)
+    try {
+        importAll(require.context('@/assets/icons', true, /\.svg$/))//将一个目录里面所有的文件引入到当前文件
+    } catch (error) {
+        console.log(error)
+    }
+    export default {
+        name: 'Icon',
+        components: {},
+
+        props: {
+            name: {
+                type: String,
+                default() {
+                    return 'money'
+                }
+            }
+        },
+
+        data() {
+            return {}
+        },
+
+
+        methods: {}
+    }
+</script>
+
+<style lang="scss" scoped>
+
+    .icon {
+        width: 1em;
+        height: 1em;
+        vertical-align: -0.15em;
+        fill: currentColor;
+        overflow: hidden;
+    }
+</style>
--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,14 +1,24 @@
 <template>
     <div class="nav-box">
-        <router-link to="/money">记账</router-link>
+        <router-link class="link" to="/money">
+            <Icon name="money"></Icon>
+            记账
+        </router-link>
         |
-        <router-link to="/labels">标签</router-link>
+        <router-link class="link" to="/labels">
+            <Icon name="label"></Icon>
+            标签
+        </router-link>
         |
-        <router-link to="/statis">统计</router-link>
+        <router-link class="link" to="/statis">
+            <Icon name="statis"></Icon>
+            统计
+        </router-link>
     </div>
 </template>
 
 <script lang="ts">
+
     export default {
         name: 'Nav',
         components: {},
@@ -25,7 +35,11 @@
 
 <style lang="scss" scoped>
     .nav-box {
+        display: flex;
         border: 1px solid red;
+        .link {
+            display: flex;
+        }
     }
 
 </style>
