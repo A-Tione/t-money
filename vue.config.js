fc7cd9fe3de6c7f1e2e80dc724a315433e7e5dcb
a-tione <heyanggo@gmail.com>
2020-03-12 15:57:45 +0800

    解决svg原有fill颜色冲突的BUG

--- vue.config.js
+++ vue.config.js
@@ -1,18 +1,21 @@
 const path = require('path')
 module.exports = {
-  lintOnSave: false,
-  chainWebpack: config => {
-    const dir = path.resolve(__dirname, 'src/assets/icons')
-    config.module
-        .rule('svg-sprite')
-        .test(/\.svg$/)
-        .include.add(dir).end()
-        .use('svg-sprite-loader')
-        .loader('svg-sprite-loader')
-        .options({extract: false}).end()
-    config.plugin('svg-sprite').use(require('svg-sprite-loader/plugin'), [{plainSprite: true}])
-    config.module.rule('svg').exclude.add(dir)//其他svg loader排除icons目录
-  },
+    lintOnSave: false,
+    chainWebpack: config => {
+        const dir = path.resolve(__dirname, 'src/assets/icons')
+        config.module
+            .rule('svg-sprite')
+            .test(/\.svg$/)
+            .include.add(dir).end()
+            .use('svg-sprite-loader')
+            .loader('svg-sprite-loader')
+            .options({extract: false}).end()
+            .use('svgo-loader').loader('svgo-loader')
+            .tap(options => ({...options, plugins: [{removeAttrs: {attrs: 'fill'}}]}))//去掉svg原有的fill颜色
+            .end()
+        config.plugin('svg-sprite').use(require('svg-sprite-loader/plugin'), [{plainSprite: true}])
+        config.module.rule('svg').exclude.add(dir)//其他svg loader排除icons目录
+    },
 
 
 }
