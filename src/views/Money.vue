e862b1ca85f5a50a57247e419259fe2aa80326fa
a-tione <heyanggo@gmail.com>
2020-03-13 15:28:34 +0800

    完成Tags组件

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,7 +1,8 @@
 <template>
     <div>
         <Layout>
-            <tags></tags>
+            <!--        sync修饰符，可直接修改子组件的source，无需另加方法-->
+            <tags :source.sync="tags"></tags>
             <notes></notes>
             <types xxx="5555"></types>
             <number-pad></number-pad>
@@ -10,10 +11,10 @@
 </template>
 
 <script lang="ts">
-    import notes from '@/components/Money/Notes.vue'
-    import numberPad from '@/components/Money/NumberPad.vue'
-    import tags from '@/components/Money/Tags.vue'
-    import types from '@/components/Money/Types.vue'
+    import notes from '@/components/Money/Notes.vue';
+    import numberPad from '@/components/Money/NumberPad.vue';
+    import tags from '@/components/Money/Tags.vue';
+    import types from '@/components/Money/Types.vue';
 
     export default {
         name: 'money',
@@ -24,11 +25,13 @@
             numberPad,
         },
         data() {
-            return {}
+            return {
+                tags: ['衣', '食', '住', '行', '长辈']
+            };
         },
 
         methods: {}
-    }
+    };
 </script>
 
 <style lang="scss" scoped>
