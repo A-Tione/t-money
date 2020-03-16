96aee88b7fc342b965fbe92e843b64018de67330
a-tione <heyanggo@gmail.com>
2020-03-16 17:15:00 +0800

    deploy

---
+++ precache-manifest.1934bd4786922042fe3e65ac65cc1683.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b09a48f71ab3112dc99c",
+    "url": "/t-money/css/app.f5bbe4a3.css"
+  },
+  {
+    "revision": "19730a29af96a9b230eef56c94b2cf05",
+    "url": "/t-money/index.html"
+  },
+  {
+    "revision": "b09a48f71ab3112dc99c",
+    "url": "/t-money/js/app.7a474895.js"
+  },
+  {
+    "revision": "a497431bfd404f42c8a0",
+    "url": "/t-money/js/chunk-vendors.550eee94.js"
+  },
+  {
+    "revision": "9722177ea70535649490ce180f90ba45",
+    "url": "/t-money/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/t-money/robots.txt"
+  }
+]);
